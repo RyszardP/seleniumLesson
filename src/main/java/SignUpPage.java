875226c745b47331e8928fb7644c82525cab623c
Rysard Poplavskij <rpoplavskij@gmail.com>
2020-11-15 16:21:01 +0300

    commit with all fields

--- src/main/java/SignUpPage.java
+++ src/main/java/SignUpPage.java
@@ -19,8 +19,11 @@ public class SignUpPage {
                 "%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D1%8C&returnto=%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F+" +
                 "%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0");
         webDriver.findElement(By.xpath("//*[@id='wpName2']")).sendKeys("name");
-        // By nameField = By.cssSelector("//*[@id=\"wpPassword2\"]");
-        // By passwordField = By.cssSelector("input#lname");
+        webDriver.findElement(By.xpath("//*[@id='wpPassword2']")).sendKeys("asdf");
+        webDriver.findElement(By.xpath("//*[@id='wpRetype']")).sendKeys("asdf");
+        webDriver.findElement(By.xpath("//*[@id='wpEmail']")).sendKeys("example@example.com");
+
+
 
     }
 }
