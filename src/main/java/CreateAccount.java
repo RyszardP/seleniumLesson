edee3fb66843dd839e07fc9c1fe534a2e4a67cf0
Rysard Poplavskij <rpoplavskij@gmail.com>
2020-11-16 19:41:54 +0300

    commit with eng version

--- src/main/java/CreateAccount.java
+++ src/main/java/CreateAccount.java
@@ -1,10 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class SignUpPage {
+public class CreateAccount {
     public static void main(String[] args) {
 
         System.setProperty("webdriver.gecko.driver", "D:\\IT\\geckodriver\\geckodriver.exe");
@@ -14,16 +15,15 @@ public class SignUpPage {
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         webDriver.manage().window().maximize();
 
-        webDriver.get("https://ru.wikipedia.org/w/index.php?title=%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0" +
-                "%A1%D0%BE%D0%B7%D0%B4%D0%B0%D1%82%D1%8C_%D1%83%D1%87%D1%91%D1%82%D0%BD%D1%83%D1%8E_" +
-                "%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D1%8C&returnto=%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F+" +
-                "%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0");
+        webDriver.get("https://en.wikipedia.org/w/index.php?title=Special:CreateAccount&returnto=Wikipedia");
         webDriver.findElement(By.xpath("//*[@id='wpName2']")).sendKeys("name");
         webDriver.findElement(By.xpath("//*[@id='wpPassword2']")).sendKeys("asdf");
         webDriver.findElement(By.xpath("//*[@id='wpRetype']")).sendKeys("asdf");
         webDriver.findElement(By.xpath("//*[@id='wpEmail']")).sendKeys("example@example.com");
+        webDriver.findElement(By.xpath("//*[@id='mw-input-captchaWord']")).sendKeys("capcha");
 
-
-
+        WebElement link = webDriver.findElement(By.xpath("//li[@id='n-mainpage-description']/a"));
+        System.out.println(link.getText());
+        link.click();
     }
 }
